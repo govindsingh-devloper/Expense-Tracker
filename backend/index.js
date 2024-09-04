9fdeca65a56683d98aeba95cf2df23d3e676c8e1
govindsingh-devloper <152960266+govindsingh-devloper@users.noreply.github.com>
2024-09-04 00:25:54 -0700

    Cors Path Change

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ app.use(cookieParser());
 
 app.use(
     cors({
-        origin:"http://localhost:3000",
+        origin:"'https://expense-tracker-frontend-murex.vercel.app'",
         credentials:true
     })
 )
