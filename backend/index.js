9ccbb1e3d2e2583e554baefec5eed3ce936a0a49
govindsingh-devloper <152960266+govindsingh-devloper@users.noreply.github.com>
2024-09-04 00:51:21 -0700

    Previous one

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ app.use(cookieParser());
 
 app.use(
     cors({
-        origin:"https://expense-tracker-1-uvvc.onrender.com",
+        origin:"http://localhost:3000",
         credentials:true
     })
 )
